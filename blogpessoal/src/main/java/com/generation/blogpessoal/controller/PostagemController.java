dbba82d3bc6c05f1d721252ab4e4d53f18699c5f
Marina_Varela <marinavareladev@gmail.com>
2022-08-04 12:10:47 -0300

    Blog Pessoal

--- blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,14 +1,52 @@
 package com.generation.blogpessoal.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogpessoal.model.PostagemModel;
+import com.generation.blogpessoal.repository.PostagemRepository;
 
 @RestController 
 @RequestMapping ("/postagens")
 @CrossOrigin("*")
+
 public class PostagemController {
 
+	@Autowired
+	private PostagemRepository postagemRepository;
+		
+	@GetMapping
+	public ResponseEntity <List<PostagemModel>> getAll(){
+			return ResponseEntity.ok(postagemRepository.findAll());
+	}
 	
+	@GetMapping ("/{id}")
+	public ResponseEntity <PostagemModel> getById(@PathVariable Long id){
+		return postagemRepository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity <List<PostagemModel>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity <PostagemModel> postPostagem(@Valid @RequestBody PostagemModel postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
 	
 }
